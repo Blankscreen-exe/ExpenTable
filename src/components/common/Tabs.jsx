e87c7dc12cfff9b3d3087e9ef63efb134bc2259d
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-10-20 01:32:19 +0500

    add tab active color

--- src/components/common/Tabs.jsx
+++ src/components/common/Tabs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import appConstants from "../../appConstants";
+import { combineClassNames } from "../../helpers";
 
 function Tabs() {
     const currentPath = useLocation().pathname;
@@ -9,14 +10,14 @@ function Tabs() {
     <div className="tabs-container">
       <div className="tabs is-centered">
         <ul>
-          <li className={currentPath==appConstants.routes.home ? "is-active has-text-primary" : ""}>
+          <li className={currentPath==appConstants.routes.home ? "is-active is-active-tab" : ""}>
             <NavLink to={appConstants.routes.home} >Home</NavLink>
           </li>
-          <li className={currentPath==appConstants.routes.about ? "is-active" : ""}>
-            <NavLink to={appConstants.routes.about} >About</NavLink>
+          <li className={currentPath==appConstants.routes.form ? "is-active is-active-tab" : ""}>
+            <NavLink to={appConstants.routes.form} >Edit Tasks</NavLink>
           </li>
-          <li className={currentPath==appConstants.routes.form ? "is-active" : ""}>
-            <NavLink to={appConstants.routes.form} >Form</NavLink>
+          <li className={currentPath==appConstants.routes.about ? "is-active is-active-tab" : ""}>
+            <NavLink to={appConstants.routes.about} >About</NavLink>
           </li>
         </ul>
       </div>
