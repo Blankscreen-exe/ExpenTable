f721b10494fd59b5e8d46c706a13e2836deb96fa
Angel Henriquez <angelhm20@hotmail.com>
2025-05-08 16:47:20 -0400

    Fix priority bug and make progress on the form style

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -13,25 +13,24 @@ function Form() {
     const [days, setDays] = useState(selectedCategory?.days || {});
     const [priorityValue, setPriority] = useState(selectedCategory?.priority || "");
 
+    console.log(priorityValue);
+
     // All days of the week
     const weekDays = appConstants.days;
     const fullWeekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    // Checks if the category has specific day
-    // const checkCategoryDay = (day) => {
-    //     const itemDay = selectedCategory.days[day];
-    //     if (itemDay) {
-    //         return itemDay;
-    //     } else {
-    //         return null;
-    //     }
-    // }
+    // Styles for the form
+    const formStyles = {
+        border: "1px solid rgb(0, 209, 178)",
+        padding: "2rem",
+    }
 
     // Change category when user selects a different one in the dropdown
     const handleSelectChange = (e) => {
         const categorySelected = formData.find(category => category.title === e.target.value);
         setCategory(categorySelected);
         setDays(categorySelected.days);
+        setPriority(categorySelected.priority);
     }
 
     // Change days state when any input triggers the onChange
@@ -47,44 +46,35 @@ function Form() {
         }
     }
 
-    // Days state
-    // const [sunday, setSunday] = useState(() => checkCategoryDay("sun"));
-    // const [monday, setMonday] = useState(() => checkCategoryDay("mon"));
-    // const [tuesday, setTuesday] = useState(() => checkCategoryDay("tue"));
-    // const [wednesday, setWednesday] = useState(() => checkCategoryDay("wed"));
-    // const [thursday, setThursday] = useState(() => checkCategoryDay("thu"));
-    // const [friday, setFriday] = useState(() => checkCategoryDay("fri"));
-    // const [saturday, setSaturday] = useState(() => checkCategoryDay("sat"));
-
     return (
-        <div>
-            <form>
+        <section>
+            <div className='is-flex is-flex-direction-column is-align-items-center'>
                 {formData.length ?
-                    <div>
-                        <div>
+                    <form style={formStyles}>
+                        <div style={{ marginBottom: "1rem" }} className='is-flex is-justify-content-center'>
                             <div className='field is-grouped'>
                                 <div className="field is-horizontal">
                                     <div className="field-label is-normal">
                                         <label htmlFor="categories" className='label'>Category:</label>
                                     </div>
                                     <div className="field-body">
-                                        <div className='control select is-primary'>
-                                            <select className='is-radiusless' onChange={handleSelectChange} name="categories" id="categories-dropdown">
-                                                {formData.map((item, index) => {
-                                                    return (<option value={item.title} key={index}>{item.title}</option>);
-                                                })}
-                                            </select>
+                                        <div className="field has-addons">
+                                            <div className='control select is-primary'>
+                                                <select className='is-radiusless' onChange={handleSelectChange} name="categories" id="categories-dropdown">
+                                                    {formData.map((item, index) => {
+                                                        return (<option value={item.title} key={index}>{item.title}</option>);
+                                                    })}
+                                                </select>
+                                            </div>
+                                            <div className='control'>
+                                                <button
+                                                    className='button is-primary is-radiusless is-outlined'
+                                                    onClick={() => setModal(true)}
+                                                >
+                                                    Edit
+                                                </button>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <div className='control'>
-                                        <button 
-                                            className='button is-primary is-radiusless is-outlined' 
-                                            onClick={() => setModal(true)}
-                                        >
-                                            Edit
-                                        </button>
                                     </div>
                                 </div>
                                 <div className="field is-horizontal">
@@ -106,17 +96,16 @@ function Form() {
                                 </div>
                             </div>
                         </div>
-                        {weekDays.map((day, index) => {
-                            return (
-                                <div key={day}>
-                                    <h2 className='subtitle'>{fullWeekDays[index]}</h2>
-                                    <div className="field is-grouped">
-                                        <div className='field'>
-                                            <div className="field-label">
+                        <div>
+                            {weekDays.map((day, index) => {
+                                return (
+                                    <div key={day}>
+                                        <hr></hr>
+                                        <h2 className='subtitle'>{fullWeekDays[index]}</h2>
+                                        <div className="field is-grouped is-justify-content-space-between">
+                                            <div className='field'>
                                                 <label htmlFor={`${day}-title`} className='label'>Title</label>
-                                            </div>
-                                            <div className="field-body">
-                                                <div className="control">
+                                                <div className="control has-icons-left">
                                                     <input
                                                         onChange={handleDaysInputChange}
                                                         type="text"
@@ -125,15 +114,14 @@ function Form() {
                                                         value={days[day]?.title || ""}
                                                         className='input is-primary is-radiusless'
                                                     />
+                                                    <span className="icon custom-icon is-left">
+                                                        <i className="fa-regular fa-pen-to-square"></i>
+                                                    </span>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div className='field'>
-                                            <div className="field-label">
+                                            <div className='field'>
                                                 <label htmlFor={`${day}-allotedTime`} className='label'>Alloted time</label>
-                                            </div>
-                                            <div className="field-body is-align-items-center">
-                                                <div className="control">
+                                                <div className="control has-icons-left">
                                                     <input
                                                         onChange={handleDaysInputChange}
                                                         type="number" step={0.5}
@@ -142,117 +130,27 @@ function Form() {
                                                         value={days[day]?.allottedTime || ""}
                                                         className='input is-primary is-radiusless'
                                                     />
+                                                    <span className="icon custom-icon is-left">
+                                                        <i className="fa-regular fa-clock"></i>
+                                                    </span>
                                                 </div>
                                                 <span>* pomodoros</span>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
-                        {/* <div>
-                            <div>
-                                <h2>Sunday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="sun-title" id="sun-title" value={days?.sun?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="sun-allottedTime" id="sun-alloted-time" value={days?.sun?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <h2>Monday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="mon-title" id="mon-title" value={days?.mon?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="mon-allottedTime" id="mon-alloted-time" value={days?.mon?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <h2>Tuesday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="tue-title" id="tue-title" value={days?.tue?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="tue-allottedTime" id="tue-alloted-time" value={days?.tue?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <h2>Wednesday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="wed-title" id="wed-title" value={days?.wed?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="wed-allottedTime" id="wed-alloted-time" value={days?.wed?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <h2>Thursday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="thu-title" id="thu-title" value={days?.thu?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="thu-allottedTime" id="thu-alloted-time" value={days?.thu?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <h2>Friday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="fri-title" id="fri-title" value={days?.fri?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="fri-allottedTime" id="fri-alloted-time" value={days?.fri?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <h2>Saturday</h2>
-                                <div>
-                                    <label>
-                                        Title
-                                        <input type="text" name="sat-title" id="sat-title" value={days?.sat?.title || ""} />
-                                    </label>
-                                    <label>
-                                        Alloted time
-                                        <input type="number" step={0.5} name="sat-allottedTime" id="sat-alloted-time" value={days?.sat?.allottedTime || ""} />
-                                    </label>
-                                </div>
-                            </div>
-                        </div> */}
-                    </div>
+                                )
+                            })}
+                        </div>
+                    </form>
                     :
-                    <div>
-                        <h2>You still have no categories created.</h2>
-                        <button className='button is-success is-outlined' onClick={() => setModal(true)}>Create a new category</button>
+                    <div className='is-flex is-flex-direction-column is-align-items-center'>
+                        <h2 className='subtitle'>You still have no categories created.</h2>
+                        <button className='button is-primary is-outlined' onClick={() => setModal(true)}>Create a new category</button>
                     </div>
                 }
-            </form>
+            </div>
             {modal && <Modal />}
-        </div>
+        </section>
     )
 }
 
