1d4195dc4360706ce56e07a3efe43ef4d2ed0a30
Angel Henriquez <angelhm20@hotmail.com>
2025-07-25 19:55:06 -0400

    Feature: Implement drag-scroll to select which day user wants to go to

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -19,10 +19,13 @@ function Form({
   const size = useWindowSize();
 
   return (
-    <div className="is-flex is-flex-direction-column is-align-items-center">
+    <div className="is-flex is-flex-direction-column is-align-items-center mx-4">
       {selectedCategory ? (
         <form className="form">
-          <div style={{ paddingInline: "2rem" }} className="field is-grouped is-justify-content-space-between is-flex-wrap-wrap">
+          <div
+            style={{ paddingInline: "2rem" }}
+            className="field is-grouped is-justify-content-space-between is-flex-wrap-wrap"
+          >
             <div className="field is-horizontal">
               <div className="field-label is-normal">
                 <label htmlFor="categories" className="label">
@@ -75,70 +78,90 @@ function Form({
           </div>
           <div>
             {size.width < 601 ? (
-              <div className="is-flex is-align-items-center is-column-gap-3 px-5">
-                <button type="button" onClick={() => handleSliderChange(false)}>
-                  <i className="fa-solid fa-angle-left fa-xl"></i>
-                </button>
-                <div className={`slider-content ${sliderLoaded ? "slider-loaded" : ""}`}>
-                  <hr></hr>
-                  <h2 style={{ marginBottom: "1rem" }} className="subtitle">
-                    {fullWeekDays[sliderIndex]}
-                  </h2>
-                  <div className="field is-grouped is-flex-wrap-wrap">
-                    <div className="field title-input__field">
-                      <label
-                        htmlFor={`${weekDays[sliderIndex]}-title`}
-                        className="label"
-                      >
-                        Title
-                      </label>
-                      <div className="control has-icons-left">
-                        <Input
-                          onChange={handleDaysInputChange}
-                          name={`${weekDays[sliderIndex]}-title`}
-                          id={`${weekDays[sliderIndex]}-title`}
-                          value={
-                            selectedCategory.days[weekDays[sliderIndex]]
-                              ?.title || ""
-                          }
-                        />
-                        <span className="icon custom-icon is-left">
-                          <i className="fa-regular fa-pen-to-square"></i>
-                        </span>
-                      </div>
-                    </div>
-                    <div className="field allottedTime-input__field">
-                      <label
-                        htmlFor={`${weekDays[sliderIndex]}-allottedTime`}
-                        className="label"
-                      >
-                        Alloted time
-                      </label>
-                      <div className="control has-icons-left">
-                        <Input
-                          onChange={handleDaysInputChange}
-                          type="number"
-                          step={0.5}
-                          name={`${weekDays[sliderIndex]}-allottedTime`}
-                          id={`${weekDays[sliderIndex]}-allotted-time`}
-                          value={
-                            selectedCategory.days[weekDays[sliderIndex]]
-                              ?.allottedTime || ""
-                          }
-                          inputMode="numeric"
-                          min={0.5}
-                        />
-                        <span className="icon custom-icon is-left is-primary">
-                          <i className="fa-regular fa-clock"></i>
-                        </span>
-                      </div>
-                      <span>* pomodoros</span>
-                    </div>
+              <div>
+                <div className="drag-scroll">
+                  <div className="dragged-items">
+                    <button className="day-switch">M</button>
+                    <button className="day-switch">T</button>
+                    <button className="day-switch">W</button>
+                    <button className="day-switch">T</button>
+                    <button className="day-switch">F</button>
+                    <button className="day-switch">S</button>
+                    <button className="day-switch">S</button>
                   </div>
                 </div>
-                <button type="button" onClick={handleSliderChange}>
-                  <i className="fa-solid fa-angle-right fa-xl"></i>
-                </button>
+                <div className="is-flex is-align-items-center is-column-gap-3 px-5">
+                  <button
+                    type="button"
+                    onClick={() => handleSliderChange(false)}
+                  >
+                    <i className="fa-solid fa-angle-left fa-xl"></i>
+                  </button>
+                  <div
+                    className={`slider-content ${
+                      sliderLoaded ? "slider-loaded" : ""
+                    }`}
+                  >
+                    <hr></hr>
+                    <h2 style={{ marginBottom: "1rem" }} className="subtitle">
+                      {fullWeekDays[sliderIndex]}
+                    </h2>
+                    <div className="field is-grouped is-flex-wrap-wrap">
+                      <div className="field title-input__field">
+                        <label
+                          htmlFor={`${weekDays[sliderIndex]}-title`}
+                          className="label"
+                        >
+                          Title
+                        </label>
+                        <div className="control has-icons-left">
+                          <Input
+                            onChange={handleDaysInputChange}
+                            name={`${weekDays[sliderIndex]}-title`}
+                            id={`${weekDays[sliderIndex]}-title`}
+                            value={
+                              selectedCategory.days[weekDays[sliderIndex]]
+                                ?.title || ""
+                            }
+                          />
+                          <span className="icon custom-icon is-left">
+                            <i className="fa-regular fa-pen-to-square"></i>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="field allottedTime-input__field">
+                        <label
+                          htmlFor={`${weekDays[sliderIndex]}-allottedTime`}
+                          className="label"
+                        >
+                          Alloted time
+                        </label>
+                        <div className="control has-icons-left">
+                          <Input
+                            onChange={handleDaysInputChange}
+                            type="number"
+                            step={0.5}
+                            name={`${weekDays[sliderIndex]}-allottedTime`}
+                            id={`${weekDays[sliderIndex]}-allotted-time`}
+                            value={
+                              selectedCategory.days[weekDays[sliderIndex]]
+                                ?.allottedTime || ""
+                            }
+                            inputMode="numeric"
+                            min={0.5}
+                          />
+                          <span className="icon custom-icon is-left is-primary">
+                            <i className="fa-regular fa-clock"></i>
+                          </span>
+                        </div>
+                        <span>* pomodoros</span>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="button" onClick={handleSliderChange}>
+                    <i className="fa-solid fa-angle-right fa-xl"></i>
+                  </button>
+                </div>
               </div>
             ) : (
               weekDays.map((day, index) => {
